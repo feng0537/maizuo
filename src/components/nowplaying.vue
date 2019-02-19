503ffd49d69bf27becacf48b8d35188d08839998
feng0537 <your_registeail>
2019-02-19 16:28:06 +0800

    0219

--- src/components/nowplaying.vue
+++ src/components/nowplaying.vue
@@ -14,7 +14,7 @@
         },
         mounted(){
             axios.get("https://www.easy-mock.com/mock/5c6a6556e5014c6a2c90fe50/api/nowplaying1").then(res=>{
-                console.log(111);
+                console.log(222);
                 console.log(res);
                 this.datalist = res.data.data.films;
                 console.log(this.datalist)
